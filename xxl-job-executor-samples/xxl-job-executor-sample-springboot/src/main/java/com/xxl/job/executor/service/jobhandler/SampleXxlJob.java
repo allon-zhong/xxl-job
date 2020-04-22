e160d8a4ade88b7fdef1dcb2b1d5de303bc2e7a9
zhongerlang <822710379@qq.com>
2020-04-22 16:47:46 +0800

    修改部分配置

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/SampleXxlJob.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/SampleXxlJob.java
@@ -39,11 +39,12 @@ public class SampleXxlJob {
     @XxlJob("demoJobHandler")
     public ReturnT<String> demoJobHandler(String param) throws Exception {
         XxlJobLogger.log("XXL-JOB, Hello World.");
+        System.out.println("demoJobHandler 执行了！！！");
 
-        for (int i = 0; i < 5; i++) {
+        /*for (int i = 0; i < 5; i++) {
             XxlJobLogger.log("beat at:" + i);
             TimeUnit.SECONDS.sleep(2);
-        }
+        }*/
         return ReturnT.SUCCESS;
     }
 
@@ -58,6 +59,7 @@ public class SampleXxlJob {
         ShardingUtil.ShardingVO shardingVO = ShardingUtil.getShardingVo();
         XxlJobLogger.log("分片参数：当前分片序号 = {}, 总分片数 = {}", shardingVO.getIndex(), shardingVO.getTotal());
 
+        System.out.println("当前分片序号："+shardingVO.getIndex());
         // 业务逻辑
         for (int i = 0; i < shardingVO.getTotal(); i++) {
             if (i == shardingVO.getIndex()) {
